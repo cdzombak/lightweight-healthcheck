1a8ed09ad36d18a41a9c0279077926ee185a77a3
Chris Dzombak <chris@dzombak.com>
2021-01-05 17:12:17 -0500

    allow waiting N minutes for DOWN to clear before sending alert

--- lightweight-healthcheck.sh
+++ lightweight-healthcheck.sh
@@ -10,38 +10,63 @@ TWILIO_NUMBER="+1xxxxxxxxxx"
 TWILIO_ACCOUNT="ACxxxxx"
 TWILIO_APIKEY="xxxxx"
 LOGFILE_NAME="website"  # logs will be written to $HOME/.lightweight-healthcheck/website.log
+DELAY_MINUTES=1  # minutes to wait for clear before sending an alert
 DAILY_LIMIT=6  # max incidents reported via email/sms during the current day
 HOURLY_LIMIT=2  # max incidents reported via email/sms during the current hour
 
+
 check() {
 	# recommendations for writing a check:
-	# * use curl -s
-	# * set curl --connect-timeout
-	# * see https://stackoverflow.com/a/42873372 for notes on curl retry options
+	# - use curl -s
+	# - set curl --connect-timeout to a small number
+	# - see https://stackoverflow.com/a/42873372 for notes on curl retry options
+
 	curl -s --connect-timeout 5 --max-time 15 --retry 3 --retry-max-time 50 https://www.dzombak.com | grep -c "<title> # Chris Dzombak</title>"
 }
 
-#
-# Do not modify below this line
-#
+#################################
+# Do not modify below this line #
+#################################
 
 mkdir -p "$HOME/.lightweight-healthcheck"
-LASTSTATUS_FILE="$HOME/.lightweight-healthcheck/.$LOGFILE_NAME.status"
 LOG_FILE="$HOME/.lightweight-healthcheck/$LOGFILE_NAME.log"
 HOSTNAME=$(hostname)
 NOW=$(date +"%F %T %Z")
 NOW_D=$(date +"%F")
 NOW_H=$(date +"%F %H")
 
-OK=
+log_ok() {
+	echo "$NOW | OK" >> "$LOG_FILE"
+}
+
+log_down() {
+	echo "$NOW | DOWN" >> "$LOG_FILE"
+}
+
+log_alert() {
+	echo "$NOW | ALERT" >> "$LOG_FILE"
+}
+
+if [ ! -f "$LOG_FILE" ]; then
+	log_ok
+fi
+if ! tail -n 1 "$LOG_FILE" | grep -c "|" >/dev/null; then
+	# last log is the old format. exit and we'll finish checking next time
+	log_ok
+	exit 0
+fi
+
+IFS='|' read -ra LASTLOG <<< "$(tail -n 1 "$LOG_FILE")"
+LAST_DATE="${LASTLOG[0]}"
+LAST_STATUS=$(echo "${LASTLOG[1]}" | sed -e 's/^[[:space:]]*//')
+
+OK=false
 if check > /dev/null ; then
-	OK="1"
+	OK=true
 else
-	sleep $(( ( RANDOM % 5 )  + 1 ))s
+	sleep 5s
 	if check > /dev/null ; then
-		OK="1"
-	else
-		OK="0"
+		OK=true
 	fi
 fi
 
@@ -55,38 +80,74 @@ send_sms() {
 	fi
 }
 
+alert_would_exceed_max_allowed() {
+	local ALERTS_TODAY
+	ALERTS_TODAY=$(grep "$NOW_D" "$LOG_FILE" | grep -c "ALERT")
+	if (( ALERTS_TODAY >= DAILY_LIMIT )); then
+		return 1  # failure exit code
+	fi
+	local ALERTS_THIS_HOUR
+	ALERTS_THIS_HOUR=$(grep "$NOW_H" "$LOG_FILE" | grep -c "ALERT")
+	if (( ALERTS_THIS_HOUR >= HOURLY_LIMIT )); then
+		return 1  # failure exit code
+	fi
+	return 0  # success exit code
+}
+
 send_ok() {
+	if ! alert_would_exceed_max_allowed ; then
+		return
+	fi
 	echo -e "$THING_DESC recovered at $NOW\n\n(seen by $HOSTNAME)" | mailx -s "$EMAIL_SUBJECT - Recovered" $EMAIL_TO
 	send_sms "$THING_DESC recovered at $NOW"
 }
 
 send_alert() {
+	if ! alert_would_exceed_max_allowed ; then
+		return
+	fi
 	echo -e "$THING_DESC was DOWN at $NOW\n\n(seen by $HOSTNAME)" | mailx -s "$EMAIL_SUBJECT - ALERT" $EMAIL_TO
 	send_sms "$THING_DESC was DOWN at $NOW"
 }
 
-if ! grep -c "$OK" "$LASTSTATUS_FILE" >/dev/null ; then
-	SEND="1"
-	ALERTS_TODAY=$(grep -c "$NOW_D" "$LOG_FILE")
-	if (( ALERTS_TODAY >= (DAILY_LIMIT * 2) )); then
-		SEND="0"
-	fi
-	ALERTS_THIS_HOUR=$(grep -c "$NOW_H" "$LOG_FILE")
-	if (( ALERTS_THIS_HOUR >= (HOURLY_LIMIT * 2) )); then
-		SEND="0"
-	fi
-
-	if [[ "$OK" == "1" ]]; then
-		echo "$NOW - OK" >> "$LOG_FILE"
-		if [[ "$SEND" == "1" ]]; then
-			send_ok
-		fi
+send_alert_if_delay_passed() {
+	local EPOCH_LAST_LOG
+	local EPOCH_NOW
+	if command -v gdate >/dev/null; then
+		EPOCH_LAST_LOG=$(gdate -d "$LAST_DATE" +%s)
+		EPOCH_NOW=$(gdate -d "$NOW" +%s)
 	else
-		echo "$NOW - ALERT" >> "$LOG_FILE"
-		if [[ "$SEND" == "1" ]]; then
-			send_alert
-		fi
+		EPOCH_LAST_LOG=$(date -d "$LAST_DATE" +%s)
+		EPOCH_NOW=$(date -d "$NOW" +%s)
+	fi
+	local DIFF
+	DIFF=$(( (EPOCH_NOW - EPOCH_LAST_LOG + 1) / 60))
+	if (( DIFF >= DELAY_MINUTES )); then
+		send_alert
+		log_alert
+	fi
+}
+
+if [ "$OK" == true ]; then
+	if [ "$LAST_STATUS" = "OK" ]; then
+		exit 0
+	elif [ "$LAST_STATUS" = "DOWN" ]; then
+		log_ok
+	elif [ "$LAST_STATUS" = "ALERT" ]; then
+		log_ok
+		send_ok
+	else
+		log_ok
+	fi
+else
+	if [ "$LAST_STATUS" = "OK" ]; then
+		send_alert_if_delay_passed
+		log_down
+	elif [ "$LAST_STATUS" = "DOWN" ]; then
+		send_alert_if_delay_passed
+	elif [ "$LAST_STATUS" = "ALERT" ]; then
+		exit 0
+	else
+		log_down
 	fi
 fi
-
-echo "$OK" > "$LASTSTATUS_FILE"
